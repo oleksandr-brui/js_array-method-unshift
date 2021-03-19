a174a9ab2245b399ce046e0bdc1ecf7417751220
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-19 13:06:33 +0200

    Solution

--- js-arrayMethodUnshift.js
+++ js-arrayMethodUnshift.js
@@ -1,1 +1,21 @@
 'use strict';
+
+numbers.unshift = function(...values) {
+  // write code here
+  let { length: arrLength } = this;
+  const { length: valuesLength } = values;
+
+  arrLength += valuesLength;
+
+  const lastindexArr = arrLength - 1;
+
+  for (let i = 0; i < arrLength; i++) {
+    this[lastindexArr - i] = this[lastindexArr - valuesLength - i];
+  }
+
+  for (let j = 0; j < valuesLength; j++) {
+    this[j] = values[j];
+  }
+
+  return arrLength;
+};
